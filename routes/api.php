ae503b4ad883cb7b6dd8c67d4e39ff2aadec7472
wassim205 <wassimelmorabit7@gmail.com>
2025-03-13 11:08:05 +0000

    CRUD for users

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\V1\Admin\AuthController;
 use App\Http\Controllers\Api\V1\Admin\CategoryController;
 use App\Http\Controllers\Api\V1\Admin\DashboardController;
 use App\Http\Controllers\Api\V1\Admin\ProductController;
+use App\Http\Controllers\Api\V1\Admin\UserController;
 
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::get('/user', function (Request $request) {
@@ -21,16 +22,27 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('admin/logout', [AuthController::class, 'logout']);
         Route::get('admin/dashboard', [DashboardController::class, 'index']);
+       
+        // Products Routes
         Route::get('admin/products', [ProductController::class, 'index']);
         Route::get('admin/products/{id}', [ProductController::class, 'show']);
         Route::post('admin/products', [ProductController::class, 'store']);
         Route::put('admin/products/{id}', [ProductController::class, 'update']);
         Route::delete('admin/products/{id}', [ProductController::class, 'destroy']);
+       
+        // Categories Routes
         Route::get('admin/categories', [CategoryController::class, 'index']);
         Route::get('admin/categories/{id}', [CategoryController::class, 'show']);
         Route::post('admin/categories', [CategoryController::class, 'store']);
         Route::put('admin/categories/{id}', [CategoryController::class, 'update']);
         Route::delete('admin/categories/{id}', [CategoryController::class, 'destroy']);
+        
+        // Users Routes
+        Route::get('admin/users', [UserController::class, 'index']);
+        Route::get('admin/users/{id}', [UserController::class, 'show']);
+        Route::post('admin/users', [UserController::class, 'store']);
+        Route::put('admin/users/{id}', [UserController::class, 'update']);
+        Route::delete('admin/users/{id}', [UserController::class, 'destroy']);
         
     });
 });
