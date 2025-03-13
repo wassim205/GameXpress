7e60e322d481d52e725c3ec594959b051dc38b45
wassim205 <wassimelmorabit7@gmail.com>
2025-03-13 09:45:20 +0000

    CRUD for categories

--- app/Http/Controllers/Api/V1/Admin/CategoryController.php
+++ app/Http/Controllers/Api/V1/Admin/CategoryController.php
@@ -2,9 +2,112 @@
 
 namespace App\Http\Controllers\Api\V1\Admin;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController
 {
-    //
+    public function index(Request $request)
+    {
+
+        if (!$request->user()->can('view_categories')) {
+            return response()->json([
+                'message' => 'You do not have permission to view categories',
+            ]);
+        } else {
+            $categories = Category::all();
+            if ($categories->isNotEmpty()) {
+                return response()->json([
+                    'categories' => $categories,
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'No categories found',
+                ]);
+            }
+        }
+    }
+
+    public function show(Request $request, $id)
+    {
+        if (!$request->user()->can('view_categories')) {
+            return response()->json([
+                'message' => 'You do not have permission to view categories',
+            ]);
+        } else {
+            $categories = Category::find($id);
+            if ($categories) {
+                return response()->json([
+                    'categories' => $categories,
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'No categories found',
+                ]);
+            }
+        }
+    }
+
+    public function store(Request $request)
+    {
+        if (!$request->user()->can('create_categories')) {
+            return response()->json([
+                'message' => 'You do not have permission to create categories',
+            ]);
+        } else {
+            $request->validate(
+                [
+                    'name' => 'required',
+                    'slug' => 'required',
+                ],
+            );
+            $category = Category::create($request->all());
+            return response()->json([
+                'category' => $category,
+            ]);
+        }
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        if (!$request->user()->can('edit_categories')) {
+            return response()->json([
+                'message' => 'You do not have permission to update categories',
+            ]);
+        } else {
+            $category = Category::find($id);
+            if ($category) {
+                $category->update($request->all());
+                return response()->json([
+                    'category' => $category,
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'No category found',
+                ]);
+            }
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        if (!$request->user()->can('delete_categories')) {
+            return response()->json([
+                'message' => 'You do not have permission to delete categories',
+            ]);
+        } else {
+            $category = Category::find($id);
+            if ($category) {
+                $category->delete();
+                return response()->json([
+                    'message' => 'Category deleted successfully',
+                ]);
+            } else {
+                return response()->json([
+                    'message' => 'No category found',
+                ]);
+            }
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Admin\AuthController;
+use App\Http\Controllers\Api\V1\Admin\CategoryController;
 use App\Http\Controllers\Api\V1\Admin\DashboardController;
 use App\Http\Controllers\Api\V1\Admin\ProductController;
 
@@ -25,6 +26,11 @@ Route::prefix('v1')->group(function () {
         Route::post('admin/products', [ProductController::class, 'store']);
         Route::put('admin/products/{id}', [ProductController::class, 'update']);
         Route::delete('admin/products/{id}', [ProductController::class, 'destroy']);
+        Route::get('admin/categories', [CategoryController::class, 'index']);
+        Route::get('admin/categories/{id}', [CategoryController::class, 'show']);
+        Route::post('admin/categories', [CategoryController::class, 'store']);
+        Route::put('admin/categories/{id}', [CategoryController::class, 'update']);
+        Route::delete('admin/categories/{id}', [CategoryController::class, 'destroy']);
         
     });
 });
