8e434f67da29268022b976f7254155dde62087c3
wassim205 <wassimelmorabit7@gmail.com>
2025-03-20 09:18:03 +0000

    GX-9 modification sur roles

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Admin\AuthController;
@@ -28,25 +29,30 @@ Route::prefix('v1')->group(function () {
             Route::get('admin/dashboard', [DashboardController::class, 'index']);
         });
         // Products Routes
-        Route::get('admin/products', [ProductController::class, 'index']);
-        Route::get('admin/products/{id}', [ProductController::class, 'show']);
-        Route::post('admin/products', [ProductController::class, 'store']);
-        Route::put('admin/products/{id}', [ProductController::class, 'update']);
-        Route::delete('admin/products/{id}', [ProductController::class, 'destroy']);
+        Route::middleware('role:super_admin, product_manager')->group(function () {
 
-        // Categories Routes
-        Route::get('admin/categories', [CategoryController::class, 'index']);
-        Route::get('admin/categories/{id}', [CategoryController::class, 'show']);
-        Route::post('admin/categories', [CategoryController::class, 'store']);
-        Route::put('admin/categories/{id}', [CategoryController::class, 'update']);
-        Route::delete('admin/categories/{id}', [CategoryController::class, 'destroy']);
+            Route::get('admin/products', [ProductController::class, 'index']);
+            Route::get('admin/products/{id}', [ProductController::class, 'show']);
+            Route::post('admin/products', [ProductController::class, 'store']);
+            Route::put('admin/products/{id}', [ProductController::class, 'update']);
+            Route::delete('admin/products/{id}', [ProductController::class, 'destroy']);
+            // Categories Routes
+            Route::get('admin/categories', [CategoryController::class, 'index']);
+            Route::get('admin/categories/{id}', [CategoryController::class, 'show']);
+            Route::post('admin/categories', [CategoryController::class, 'store']);
+            Route::put('admin/categories/{id}', [CategoryController::class, 'update']);
+            Route::delete('admin/categories/{id}', [CategoryController::class, 'destroy']);
+        });
 
         // Users Routes
-        Route::get('admin/users', [UserController::class, 'index']);
-        Route::get('admin/users/{id}', [UserController::class, 'show']);
-        Route::post('admin/users', [UserController::class, 'store']);
-        Route::put('admin/users/{id}', [UserController::class, 'update']);
-        Route::delete('admin/users/{id}', [UserController::class, 'destroy']);
+        Route::middleware('role:super_admin, user_manager')->group(function () {
+
+            Route::get('admin/users', [UserController::class, 'index']);
+            Route::get('admin/users/{id}', [UserController::class, 'show']);
+            Route::post('admin/users', [UserController::class, 'store']);
+            Route::put('admin/users/{id}', [UserController::class, 'update']);
+            Route::delete('admin/users/{id}', [UserController::class, 'destroy']);
+        });
 
         //Orders Rotes
         Route::get('admin/orders', [OrderController::class, 'index']);
@@ -56,15 +62,14 @@ Route::prefix('v1')->group(function () {
 
         //Pyemennt Routes
         Route::get('admin/payments', [PaymentController::class, 'index']);
-        Route::get('admin/payments/{id}', [PaymentController::class,'show']);
-        Route::post('admin/payments', [PaymentController::class,'store']);
-
+        Route::get('admin/payments/{id}', [PaymentController::class, 'show']);
+        Route::post('admin/payments', [PaymentController::class, 'store']);
     });
 });
 
-Route::prefix('v2')->group(function(){
-    Route::get('cart',[CartController::class,'index']);
-    Route::put('cart/update',[CartController::class,'update']);
-    Route::delete('cart/delete',[CartController::class,'delete']);
+Route::prefix('v2')->group(function () {
+    Route::get('cart', [CartController::class, 'index']);
+    Route::put('cart/update', [CartController::class, 'update']);
+    Route::delete('cart/delete', [CartController::class, 'delete']);
     Route::post('/cart', [CartController::class, 'store'])->name('user.store');
 });
